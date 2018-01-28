4c73a2237fa7ed7d5d19a259b882826241a39a27
Jason-Gew <jason.ge.wu@gmail.com>
2018-01-28 14:34:46 -0600

    Delete redundant file

--- MqttPubSubClient/src/main/java/gew/PubSub/mqtt/DefaultClient.java
+++
@@ -1,9 +0,0 @@
-package gew.PubSub.mqtt;
-
-public interface DefaultClient
-{
-    boolean isConnected();
-    Boolean initialze();
-    boolean connect() throws Exception;
-    void disconnect();
-}
